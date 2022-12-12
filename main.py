fe760efe25f42e294a3f3bbec7afddfee01030ab
gFrancoCamilo <gustavocamilo@poli.ufrj.br>
2022-12-12 09:29:12 -0300

    Added more tests in main.py

--- main.py
+++ main.py
@@ -1,19 +1,48 @@
 from topology import *
 from prefferential_attachment import *
 from random import randint
+import pickle
 
 def main ():
     filenames, dates = generate_timestamps()
     Graph = create_graph(filenames[0])
-    node_diff = get_node_diff(create_graph(filenames[1]), Graph)
+    node_diff = list(get_node_diff(create_graph(filenames[1]), create_graph(filenames[0])))[-1:]
 
-    transitivity = []
+    transitivity_file = open("transitivity", "w")
+    nodes_file = open("selected_nodes","wb")
+    reward_file = open("reward","wb")
 
     for node in node_diff:
-        transitivity.append(nx.transitivity(Graph))
-        incremental_closeness (Graph, node, randint(2,6))
-        transitivity.append(nx.transitivity(Graph))
+        transitivity = (nx.transitivity(Graph))
+        transitivity_file.write(str(transitivity))
+        selected_node, reward = incremental_closeness (Graph, node, 2)
+        pickle.dump(selected_node, nodes_file)
+        pickle.dump(reward, reward_file)
+
+        transitivity = (nx.transitivity(Graph))
+        transitivity_file.write(str(transitivity))
+        selected_node, reward = incremental_closeness (Graph, node, 3)
+        pickle.dump(selected_node, nodes_file)
+        pickle.dump(reward, reward_file)
+        
+        transitivity = (nx.transitivity(Graph))
+        transitivity_file.write(str(transitivity))
+        selected_node, reward = incremental_closeness (Graph, node, 4)
+        nodes_file.write(selected_node)
+        reward_file.write(reward)
+        
+        transitivity = (nx.transitivity(Graph))
+        transitivity_file.write(str(transitivity))
+        selected_node, reward = incremental_closeness (Graph, node, 5)
+        pickle.dump(selected_node, nodes_file)
+        pickle.dump(reward, reward_file)
+
+        transitivity = (nx.transitivity(Graph))
+        transitivity_file.write(str(transitivity))
     
-    transitivity_file = open("transitivity", "w")
-    transitivity_file.write(transitivity)
+    nx.write_graphml(Graph, 'graph_after')
     transitivity_file.close()
+    nodes_file.close()
+    reward_file.close()
+
+main()
