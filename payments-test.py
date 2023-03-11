3dbfd97335d7239e300e3e23233504ab5ebf6697
gFrancoCamilo <gustavocamilo@poli.ufrj.br>
2023-03-11 11:45:20 -0300

    Added function to select payments from dataset

--- payments-test.py
+++ payments-test.py
@@ -9,4 +9,12 @@ def get_payment_dataset_test ():
     except:
         raise Exception ('Failed to load payments dataset')
 
-get_payment_dataset_test()
+def choose_payments_test (n_payments: int = 10):
+    try:
+        dataset = get_payment_dataset()
+        choose_payments(dataset, n_payments)
+    except:
+        raise Exception ('Failed to select payments')
+
+#get_payment_dataset_test()
+choose_payments_test(100)
--- payments.py
+++ payments.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 from scipy import stats
 
+ONE_DOLLAR_IN_SATOSHI = 4968
+
 def get_payment_dataset (option: str = 'ripple'):
     if option == 'ripple':
         payments = pd.read_csv('datasets/transactions-in-USD-jan-2013-aug-2016.txt')
@@ -19,5 +21,28 @@ def get_payment_dataset (option: str = 'ripple'):
 
 def filter_data (payments: pd.DataFrame):
     payments = payments.loc[payments < 10**(8)]
+    payments = payments.loc[payments > 0]
     payments = payments[(np.abs(stats.zscore(payments)) < 3)]
+    return payments
+
+def get_end_hosts (Graph: nx.DiGraph, n: int = 4):
+    end_hosts = []
+    for node in Graph.nodes():
+        if Graph.degree(node) <= 4:
+            end_hosts.append(node)
+    return end_hosts
+
+def choose_payments (payment_data: pd.DataFrame, n_payments: int = 10):
+    payments = 0
+    payment_values = []
+    
+    while payments < n_payments:
+        payment_values.append(np.random.choice(payment_data.to_list()))
+        payments += 1
+    payments_satoshis = convert_dolars_to_satoshi(payment_values)
+    return payments_satoshis
+
+def convert_dolars_to_satoshi (payments: list) -> list:
+    for index in range(len(payments)):
+        payments[index] = round(payments[index] * ONE_DOLLAR_IN_SATOSHI)
     return payments
